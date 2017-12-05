e15c5680e587787598b760a5476c2ecb5b57fc97
Nico De Witte <nico.dewitte@outlook.com>
2017-12-05 21:20:55 +0100

    Add reset and calibrate. Also use smb command methods

--- touchberrypi/touch_sensor.py
+++ touchberrypi/touch_sensor.py
@@ -1,8 +1,10 @@
 class TouchSensor(object):
-    I2C_ADDRESS = 0x1b
+    I2C_ADDRESS = 0x1B
     CHIP_ID_CHECK = 0x2E
-    REG_CHIP_ID = 0x00
-    REG_KEY_STATE = 0x03
+    REG_CHIP_ID = 0
+    REG_KEY_STATE = 3
+    REG_CALIBRATE = 56
+    REG_RESET = 57
 
     def __init__(self, i2c_bus):
         self.i2c_bus = i2c_bus
@@ -14,9 +16,17 @@ class TouchSensor(object):
     def key_state(self):
         return self.read_register(TouchSensor.REG_KEY_STATE)
 
+    def calibrate(self):
+        self.write_register(TouchSensor.REG_CALIBRATE, 0xFF)
+
+    def reset(self):
+        self.write_register(TouchSensor.REG_RESET, 0xFF)
+
+    def write_register(self, register, value):
+        self.i2c_bus.write_byte_data(TouchSensor.I2C_ADDRESS, register, value)
+
     def read_register(self, register):
-        self.i2c_bus.write_byte(TouchSensor.I2C_ADDRESS, register)
-        return self.i2c_bus.read_byte(TouchSensor.I2C_ADDRESS)
+        return self.i2c_bus.read_byte_data(TouchSensor.I2C_ADDRESS, register)
 
     def self_check(self):
         if self.chip_id() != TouchSensor.CHIP_ID_CHECK:
