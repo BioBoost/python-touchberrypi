34d61cc894c1a4c6040d7ea57645df5779abeb8f
Nico De Witte <nico.dewitte@vives.be>
2017-12-12 10:54:34 +0100

    Add some more colors to Colors class

--- touchberrypi/color.py
+++ touchberrypi/color.py
@@ -26,3 +26,8 @@ class Colors(object):
     RED = Color(255, 0, 0)
     GREEN = Color(0, 255, 0)
     BLUE = Color(0, 0, 255)
+    WHITE = Color(255, 255, 255)
+    BLACK = Color(0, 0, 0)
+    YELLOW = Color(255, 255, 0)
+    MAGENTA = Color(255, 0, 255)
+    CYAN = Color(0, 255, 255)
